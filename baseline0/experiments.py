93e75c29e49871d81f499b9ff0ab458d16ba4002
sotir <sotiriskot9@gmail.com>
2018-11-17 23:22:19 +0200

    updates

--- baseline0/experiments.py
+++ baseline0/experiments.py
@@ -285,9 +285,9 @@ def main(args):
     print(
         'Train graph created: {} nodes, {} edges'.format(train_graph.number_of_nodes(), train_graph.number_of_edges()))
     print('Number of connected components: ', nx.number_connected_components(train_graph))
-    G = node2vec.Graph(train_graph, args.directed, args.p, args.q)
-    G.preprocess_transition_probs()
-    walks = G.simulate_walks(args.num_walks, args.walk_length)
+    # G = node2vec.Graph(train_graph, args.directed, args.p, args.q)
+    # G.preprocess_transition_probs()
+    # walks = G.simulate_walks(args.num_walks, args.walk_length)
     walks = pickle.load(open('/home/paperspace/sotiris/thesis/walks.p', 'rb'))
     # walks = [['1', '23345', '3356', '4446', '5354', '6123', '74657', '8445', '97890', '1022', '1133'],
     #          ['6914', '1022', '97890', '8445', '74657', '6123', '5354', '4446', '3356', '23345', '1'],
