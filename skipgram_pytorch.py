5fa9a3bb6198626f2bea183efc7bd2c1c13f13d2
sotir <sotiriskot9@gmail.com>
2018-11-17 22:00:20 +0200

    updates

--- skipgram_pytorch.py
+++ skipgram_pytorch.py
@@ -24,35 +24,38 @@ class SkipGram(nn.Module):
         self.v_embeddings.weight.data.uniform_(-0, 0)
 
     def get_average_embedings(self, pos_u, pos_v, neg_v):
-        pos_u_average = torch.Tensor(self.batch_size, self.embedding_dim)
-        for idx, phrase_idxs in enumerate(pos_u):
+        pos_u_average = []
+        for phrase_idxs in pos_u:
             embed_u = self.u_embeddings(phrase_idxs)
             if len(phrase_idxs) > 1:
                 embed = torch.sum(embed_u, dim=0)
-                pos_u_average[idx] = embed.div(len(phrase_idxs))
+                pos_u_average.append(embed.div(len(phrase_idxs)))
             else:
-                pos_u_average[idx] = embed_u
+                pos_u_average.append(embed_u)
+        pos_u_average = torch.stack(pos_u_average)
 
-        pos_v_average = torch.Tensor(self.batch_size * 2 * self.window_size, self.embedding_dim)
-        for idx, phrase_idxs in enumerate(pos_v):
+        pos_v_average = []
+        for phrase_idxs in pos_v:
             embed_v = self.v_embeddings(phrase_idxs)
             if len(phrase_idxs) > 1:
                 embed = torch.sum(embed_v, dim=0)
-                pos_v_average[idx] = embed.div(len(phrase_idxs))
+                pos_v_average.append(embed.div(len(phrase_idxs)))
             else:
-                pos_v_average[idx] = embed_v
+                pos_v_average.append(embed_v)
+        pos_v_average = torch.stack(pos_v_average)
         pos_v_average = pos_v_average.view(self.batch_size, 2 * self.window_size, self.embedding_dim)
 
-        neg_v_average = torch.Tensor(pos_u_average.shape[0] * self.neg_sample_num, self.embedding_dim)
-        for idx, phrase_idxs in enumerate(neg_v):
+        neg_v_average = []
+        for phrase_idxs in neg_v:
             neg_embed_v = self.v_embeddings(phrase_idxs)
             if len(phrase_idxs) > 1:
                 embed = torch.sum(neg_embed_v, dim=0)
-                neg_v_average[idx] = embed.div(len(phrase_idxs))
+                neg_v_average.append(embed.div(len(phrase_idxs)))
             else:
-                neg_v_average[idx] = neg_embed_v
-
+                neg_v_average.append(neg_embed_v)
+        neg_v_average = torch.stack(neg_v_average)
         neg_v_average = neg_v_average.view(pos_u_average.shape[0], self.neg_sample_num, self.embedding_dim)
+        
         return pos_u_average, pos_v_average, neg_v_average
 
     def forward(self, pos_u, pos_v, neg_v):
