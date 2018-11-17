ae933cfbd8aba993f3bdacb2d693e050188c1fd7
sotir <sotiriskot9@gmail.com>
2018-11-17 22:42:34 +0200

    updates

--- skipgram_pytorch.py
+++ skipgram_pytorch.py
@@ -19,9 +19,9 @@ class SkipGram(nn.Module):
         self.init_emb()
 
     def init_emb(self):
-        initrange = 0.5 / self.embedding_dim
-        self.u_embeddings.weight.data.uniform_(-initrange, initrange)
-        self.v_embeddings.weight.data.uniform_(-0, 0)
+        scale = 1e-4
+        self.u_embeddings.weight.data.uniform_(-scale, scale)
+        self.v_embeddings.weight.data.uniform_(-scale, scale)
 
     def get_average_embedings(self, pos_u, pos_v, neg_v):
         pos_u_average = []
@@ -59,16 +59,37 @@ class SkipGram(nn.Module):
 
         return pos_u_average, pos_v_average, neg_v_average
 
+    def dot_product_sum(self, node_emb, ex_embeds, exp=False):
+        res = torch.bmm(ex_embeds, node_emb.unsqueeze(2)).squeeze()
+        res = (res.sum(-1))
+        if (exp):
+            res = torch.exp(res) * (res > 0.).float()
+        # in the paper they use sum not average. it is kind of strange
+        # res         = res.sum(-1) / float(res.size(0))
+        res = res.sum(-1)
+        return res
+
+    def get_loss(self, node_emb, pe_emb, ne_emb):
+        # Equation 2 of section 3 : Sum of f(ni) . f(u)
+        pos_loss = self.dot_product_sum(node_emb, pe_emb, exp=False)
+        # this is called "-log Zu" in the paper
+        neg_loss = self.dot_product_sum(node_emb, ne_emb, exp=True)
+        neg_loss = - torch.log(1 + neg_loss)
+        #
+        losss = - (neg_loss + pos_loss)
+        return losss
+
     def forward(self, pos_u, pos_v, neg_v):
         embed_u, embed_v, neg_embed_v = self.get_average_embedings(pos_u, pos_v, neg_v)
-        score = torch.bmm(embed_v, embed_u.unsqueeze(2)).squeeze()
-        log_target = F.logsigmoid(score)
-        log_target = torch.sum(log_target, dim=1)
-        neg_score = torch.bmm(neg_embed_v, embed_u.unsqueeze(2)).squeeze()
-        sum_log_sampled = F.logsigmoid(-1 * neg_score)
-        sum_log_sampled = torch.sum(sum_log_sampled, dim=1)
-        loss = log_target + sum_log_sampled
-        return -1 * loss.sum() / self.batch_size
+        loss = self.get_loss(embed_u, embed_v, neg_embed_v)
+        return loss
+        # score = torch.bmm(embed_v, embed_u.unsqueeze(2)).squeeze()
+        # score = torch.sum(score, dim=1)
+        # neg_score = torch.bmm(neg_embed_v, embed_u.unsqueeze(2)).squeeze()
+        # sum_log_sampled = F.logsigmoid(-1 * neg_score)
+        # sum_log_sampled = torch.sum(sum_log_sampled, dim=1)
+        # loss = score + sum_log_sampled
+        # return -1 * loss.sum() / self.batch_size
 
     def save_embeddings(self, file_name, idx2word, use_cuda=False):
         wv = {}
