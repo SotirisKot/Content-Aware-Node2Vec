1b75115678264e2c2daa4409198e902f14824bd4
sotir <sotiriskot9@gmail.com>
2018-12-16 19:13:19 +0200

    updates

---
+++ rnn_node2vec/rnn_batch_node2vec_pytorch.py
@@ -0,0 +1,136 @@
+import torch
+import re
+from torch.autograd import Variable
+import torch.optim as optim
+import time
+from rnn_node2vec_utils import Utils
+from rnn_batch_skipgram import node2vec_rnn
+from torch.utils.data import DataLoader
+from rnn_dataloader import Node2VecDataset
+import numpy as np
+from tqdm import tqdm
+from keras.preprocessing.sequence import pad_sequences
+
+bioclean = lambda t: ' '.join(re.sub('[.,?;*!%^&_+():-\[\]{}]', '',
+                                     t.replace('"', '').replace('/', '').replace('\\', '').replace("'",
+                                                                                                   '').strip().lower()).split()).strip()
+
+
+def tokenize(x):
+    return bioclean(x)
+
+
+def save_checkpoint(state, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+
+
+def get_index(w, vocab):
+    try:
+        return vocab[w]
+    except KeyError:
+        return vocab['UNKN']
+
+
+def phr2idx(phr, word_vocab):
+    p = [get_index(t, word_vocab) for t in phr.split()]
+    return p
+
+
+def print_params(model):
+    print(40 * '=')
+    print(model)
+    print(40 * '=')
+    total_params = 0
+    for parameter in model.parameters():
+        v = 1
+        for s in parameter.size():
+            v *= s
+        total_params += v
+    print(40 * '=')
+    print(total_params)
+    print(40 * '=')
+
+
+class Node2Vec:
+    def __init__(self, walks, output_file, walk_length, embedding_dim=128, rnn_size=50, epochs=10, batch_size=32, window_size=10,
+                 neg_sample_num=5):
+        self.utils = Utils(walks, window_size, walk_length)
+        self.vocabulary_size = self.utils.vocabulary_size
+        self.node2phr = self.utils.phrase_dic
+        self.word2idx = self.utils.word2idx
+        self.embedding_dim = embedding_dim
+        self.rnn_size = rnn_size
+        self.window_size = window_size
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.neg_sample_num = neg_sample_num
+        # self.odir_checkpoint = 'drive/My Drive/pytorch-node2vec-umls-relations/checkpoints/'
+        # self.odir_embeddings = 'drive/My Drive/pytorch-node2vec-umls-relations/embeddings/'
+        self.odir_checkpoint = '/home/paperspace/sotiris/'
+        self.odir_embeddings = '/home/paperspace/sotiris/'
+        self.output_file = output_file
+        self.wv = {}
+
+    def train(self):
+        model = node2vec_rnn(self.vocabulary_size, self.embedding_dim, self.rnn_size, self.neg_sample_num,
+                             self.batch_size,
+                             self.window_size)
+        print_params(model)
+        params = model.parameters()
+        if torch.cuda.is_available():
+            print('GPU available!!')
+            model.cuda()
+
+        optimizer = optim.Adam(params, lr=0.001)
+        dataset = Node2VecDataset(self.utils, self.neg_sample_num)
+        dataloader = DataLoader(dataset=dataset,
+                                batch_size=self.batch_size,
+                                shuffle=False,
+                                drop_last=True)
+
+        for epoch in range(self.epochs):
+            batch_num = 0
+            batch_costs = []
+            for sample in tqdm(dataloader):
+                max_phr_len = 0
+                max_pos_len = 0
+                max_neg_len = 0
+                center = sample['center']
+                context = sample['context']
+                neg_v = np.random.choice(self.utils.sample_table, size=(len(center) * self.neg_sample_num)).tolist()
+
+                phr = [phr2idx(self.utils.phrase_dic[int(phr)], self.word2idx) for phr in center]
+                max_phr_len = max([max_phr_len] + [len(pos_u) for pos_u in phr])
+
+                pos_context = [phr2idx(self.utils.phrase_dic[int(item)], self.word2idx) for item in context]
+                max_pos_len = max([max_pos_len] + [len(pos_ind) for pos_ind in pos_context])
+
+                neg_v = [phr2idx(self.utils.phrase_dic[int(item)], self.word2idx) for item in neg_v]
+                max_neg_len = max([max_neg_len] + [len(neg_ind) for neg_ind in neg_v])
+
+                batch_phr_inds = np.stack(pad_sequences(sequences=[b for b in phr], maxlen=max_phr_len))
+                batch_pos_inds = np.stack(pad_sequences(sequences=[b for b in pos_context], maxlen=max_pos_len))
+                batch_neg_inds = np.stack(pad_sequences(sequences=[b for b in neg_v], maxlen=max_neg_len))
+
+                optimizer.zero_grad()
+                loss = model(batch_phr_inds, batch_pos_inds, batch_neg_inds)
+                loss.backward()
+                optimizer.step()
+                batch_costs.append(loss.cpu().item())
+
+                if batch_num % 5000 == 0:
+                    print('Batches Average Loss: {}, Batches: {} '.format(
+                        sum(batch_costs) / float(len(batch_costs)),
+                        batch_num))
+                    batch_costs = []
+                batch_num += 1
+            print()
+            state = {'epoch': epoch + 1, 'state_dict': model.state_dict(), 'optimizer1': optimizer1.state_dict(), 'optimizer2': optimizer2.state_dict()}
+            save_checkpoint(state,
+                            filename=self.odir_checkpoint + 'part_of_rnn_checkpoint_epoch_{}.pth.tar'.format(
+                                epoch + 1))
+            self.utils.stop = True
+        print("Optimization Finished!")
+        self.wv = model.save_embeddings(file_name=self.odir_embeddings + self.output_file,
+                                        idx2word=self.utils.idx2word,
+                                        use_cuda=True)
---
+++ rnn_node2vec/rnn_batch_skipgram.py
@@ -0,0 +1,110 @@
+from pprint import pprint
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import pdb
+from tqdm import tqdm
+my_seed = 1997
+torch.manual_seed(my_seed)
+torch.cuda.manual_seed(my_seed)
+
+
+class node2vec_rnn(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, rnn_size, neg_sample_num, batch_size, window_size, scale=1e-4, max_norm=1):
+        super(node2vec_rnn, self).__init__()
+        self.u_embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
+        self.v_embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
+        self.embedding_dim = embedding_dim
+        self.neg_sample_num = neg_sample_num
+        self.batch_size = batch_size
+        self.window_size = window_size
+        self.rnn_size = rnn_size
+        print(self.rnn_size)
+        self.scale = scale
+        self.max_norm = max_norm
+        self.h0 = nn.Parameter(torch.randn(1, self.batch_size, self.rnn_size).uniform_(-self.scale, self.scale))
+        self.the_rnn = nn.GRU(input_size=self.embedding_dim, hidden_size=self.rnn_size, num_layers=1, bidirectional=False,
+                              bias=True, dropout=0, batch_first=True)
+        self.init_weights(self.scale)
+        self.init_emb()
+
+    def init_emb(self):
+        initrange = 0.5 / self.embedding_dim
+        self.u_embeddings.weight.data.uniform_(-initrange, initrange)
+        self.u_embeddings.weight.data[0] = 0
+        self.v_embeddings.weight.data.uniform_(-0, 0)
+
+    def init_weights(self, scale=1e-4):
+        for param in self.the_rnn.parameters():
+            nn.init.uniform_(param, a=-scale, b=scale)
+
+    def fix_input(self, phr_inds, pos_inds, neg_inds):
+        phr = [Variable(torch.LongTensor(phr_ind), requires_grad=False) for phr_ind in phr_inds]
+        pos = [Variable(torch.LongTensor(pos_ind), requires_grad=False) for pos_ind in pos_inds]
+        neg = [Variable(torch.LongTensor(neg_ind), requires_grad=False) for neg_ind in neg_inds]
+        return phr, pos, neg
+
+    def get_words_embeds(self, phr, pos, neg):
+        phr = [self.u_embeddings(p) for p in phr]
+        phr = torch.stack(phr)
+        pos = [self.v_embeddings(p) for p in pos]
+        pos = torch.stack(pos)
+        neg = [self.u_embeddings(n) for n in neg]
+        neg = torch.stack(neg)
+        return phr, pos, neg
+
+    def rnn_representation_one(self, inp):
+        inp, hn = self.the_rnn(inp, self.h0)
+        last_timestep = hn[-1]
+        return last_timestep
+
+    def get_rnn_representation(self, phr, pos, neg):
+        phr = self.rnn_representation_one(phr)
+        pos = self.rnn_representation_one(pos)
+
+        first_neg_batch = neg[:self.batch_size]
+        second_neg_batch = neg[self.batch_size:]
+        neg = torch.cat([self.rnn_representation_one(n) for n in [first_neg_batch, second_neg_batch]], dim=0)
+        neg = neg.view(phr.size(0), -1, self.rnn_size)
+        
+        return phr, pos, neg
+
+    def get_loss(self, phr_emb, context_emb, neg_emb):
+        score = torch.mul(phr_emb, context_emb)
+        score = torch.sum(score, dim=1)
+        log_target = F.logsigmoid(score)
+        neg_score = torch.bmm(neg_emb, phr_emb.unsqueeze(2)).squeeze()
+        sum_log_sampled = F.logsigmoid(-1 * neg_score)
+        sum_log_sampled = torch.sum(sum_log_sampled, dim=1)
+        loss = log_target + sum_log_sampled
+        return -1 * torch.mean(loss)
+
+    def forward(self, phr_inds, pos_inds, neg_inds):
+        phr, pos, neg = self.fix_input(phr_inds, pos_inds, neg_inds)
+        phr, pos, neg = self.get_words_embeds(phr, pos, neg)
+        phr, pos, neg = self.get_rnn_representation(phr, pos, neg)
+        loss = self.get_loss(phr, pos, neg)
+        return loss
+
+    def save_embeddings(self, file_name, idx2word, use_cuda=False):
+        wv = {}
+        if use_cuda:
+            embedding_u = self.u_embeddings.weight.cpu().data.numpy()
+            embedding_v = self.v_embeddings.weight.cpu().data.numpy()
+        else:
+            embedding_u = self.u_embeddings.weight.data.numpy()
+            embedding_v = self.v_embeddings.weight.data.numpy()
+
+        fout = open(file_name, 'w')
+        fout.write('%d %d\n' % (len(idx2word), self.embedding_dim))
+        for wid, w in idx2word.items():
+            e_u = embedding_u[wid]
+            e_v = embedding_v[wid]
+            e = (e_u + e_v) / 2
+            wv[w] = e
+            e = ' '.join(map(lambda x: str(x), e))
+            fout.write('%s %s\n' % (w, e))
+        return wv
